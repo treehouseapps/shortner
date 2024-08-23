ccb0619dfbaee71b27c3e4e331d1f7e54f587b33
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 17:16:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 require('dotenv').config()
-const path = require('path')
+const path = require('path');
 
-app.use(express.static(path.join(__dirname, '/public')))
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/view')
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-const routes = require('./routes/routes')
+app.set('views', path.join(__dirname, '/views'));
 
-app.use('/', routes)
+app.get('/', (req, res) => {
+  try{ res.render('index');}
+   catch(er){res.send(er)}
+});
 
-app.listen(process.env.PORT, () => {
-    console.log('Server Running in port ' + process.env.PORT)
-})
+app.listen(3000, () => {
+    console.log('Server running on port ' + 3000);
+});
