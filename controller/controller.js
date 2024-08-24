fdc0835a0371741b6524dc09fa7ed5fe56448611
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-08-24 21:30:07 +0300

    ordering

--- controller/controller.js
+++ controller/controller.js
@@ -2,37 +2,42 @@ const express = require('express')
 const app = express()
 
 const collection = require('../model/model')
-if (collection.length > 0) {
-    console.log("greater than 3")
-}
-
+// Retrve and Display from Database
 const home = async (req, res) => {
     const result = await collection.find()
     if (result.length > 0) {
         res.render('index', { result })
     }
-    else { 
+    else {
         console.log("i am in collection home")
         res.render('index', { title: " no data" })
-    console.log("i even render it")}
-}
-
-module.exports = home ;
-console.log("this is home length "+home.length)
-
-
-
-/*const express = require('express')
-const app = express()
-const collection = require('../model/model')
-
-const home = async (req, res) => {
-    try {
-        const result = await collection.find({})
-        res.render('index', { name: "", result })
+        console.log("i even render it")
     }
-    catch (ex) { console.log("error is here ==== " + ex) }
 }
+// Lead to The original Website
+const newpage = async (req, res) => {
+    const searchId = req.params.newLink
+    const result = await collection.findOne({ generated: searchId })
+    if (result) {
+        res.redirect(result.link)
+    }
+    else {
+        res.redirect('https://www.google.com')
+    }
+}
+//Remove from Database
+const remove = async (req, res) => {
+    const getid = req.params.id
+    await collection.deleteOne({ _id: getid })
+    res.redirect('/')
+}
+
+module.exports = { home, newpage, remove };
+
+
+
+/*
+
 const character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
 const shorten = async (req, res) => {
@@ -53,23 +58,6 @@ const shorten = async (req, res) => {
     }
     const result = await collection.find()
     res.render('index', { result })
-}
-
-const newpage = async (req, res) => {
-    const searchId = req.params.newLink
-    const result = await collection.findOne({ generated: searchId })
-    if (result) {
-        res.redirect(result.link)
-    }
-    else {
-        res.redirect('https://www.google.com')
-    }
-}
-
-const remove = async (req, res) => {
-    const getid = req.params.id
-    await collection.deleteOne({ _id: getid })
-    res.redirect('/')
 }
 
 module.exports = { home, shorten, newpage, remove }
