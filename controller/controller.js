ceea3ad6b553c09c4cbbb160e5cd96bba7f2bd90
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-08-24 19:33:56 +0300

    add

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,28 @@
 const express = require('express')
 const app = express()
+
+const collection = require('../model/model')
+if (collection.length > 0) {
+    console.log("greater than 3")
+}
+if (collection.length < 1) {
+    console.log("greater less than 1")
+}
+
+const home = async (req, res) => {
+    const result = await collection.find()
+    if (result.length) {
+        res.render('index', { result })
+    }
+    else { res.render('index', { title: " no data" }) }
+}
+
+module.exports = { home };
+
+
+
+/*const express = require('express')
+const app = express()
 const collection = require('../model/model')
 
 const home = async (req, res) => {
@@ -49,3 +72,4 @@ const remove = async (req, res) => {
 }
 
 module.exports = { home, shorten, newpage, remove }
+*/
--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+const routes = require('./routes/routes')
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
-app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res) => {
-  try{ res.render("index");}
-   catch(er){res.send(er)}
-});
+app.use("/", routes);
 
 app.listen(3000, () => {
-    console.log('Server running on port ' + 3000);
+  console.log('Server running on port ' + 3000);
 });
